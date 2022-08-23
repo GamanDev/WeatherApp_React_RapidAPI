fa72db70f63befbbc8cbe90ce91d48e307440a2c
Ivan-Bliminse <arturgorbunov3@gmail.com>
2022-08-23 18:40:06 +0300

    weather

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "9bf7f18825mshd66ab4f3214c6cdp1f94b1jsn2689a29c8257",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "c8f3c64122f1a525acd32c4fd682f6e6";
